fbf2508abcdd4158e07645aa28d6e33242ed6011
fscali <francesco.scali@gmail.com>
2021-08-19 12:16:56 +0200

    first app - reengineering of products in order to avoid the strong assumption that the host should have an element with a very specific id to attach to

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,9 +1,33 @@
 import faker from 'faker';
 
-let products = '';
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+const mount = (el) => {
+  let products = '';
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+  el.innerHTML = products;
+};
+
+// Context/Situation #1
+// we are running this file in development in isolation
+// We are using our local index.html file
+// which has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+
+// set automatically by WEBPACK
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products');
+
+  // Assuming our container doesn't have an element with id dev-products
+  if (el) {
+    mount(el);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// Context/Situation #2
+// We are running this file in development or production through the
+// container app and there is no guaranguee that an element with
+// an id of 'dev-products' exist
+// WE DO NOT WANT to try to immediately render the app
+export { mount };
